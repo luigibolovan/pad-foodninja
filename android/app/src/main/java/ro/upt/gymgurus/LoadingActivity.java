8385212ee9b017e1b759e7e7408e777449d8d86e
Luigi Bolovan <luigibolovan@gmail.com>
2020-04-20 20:50:20 +0300

    add dummy app only with layouts

---
+++ android/app/src/main/java/ro/upt/gymgurus/LoadingActivity.java
@@ -0,0 +1,31 @@
+package ro.upt.gymgurus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class LoadingActivity extends AppCompatActivity {
+    private final static int FAKE_LOADING_TIME_VALUE    = 5000;
+    private CountDownTimer mDummyLoadingTimer           = new CountDownTimer(FAKE_LOADING_TIME_VALUE, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            //nada
+        }
+
+        @Override
+        public void onFinish() {
+            Intent nextActivityIntent = new Intent(LoadingActivity.this, LoginActivity.class);
+            startActivity(nextActivityIntent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        mDummyLoadingTimer.start();
+    }
+}
