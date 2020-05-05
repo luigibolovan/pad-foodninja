258082b7cf4d32214b5e77f904e0ed7b7a771632
BogdanBercea <bogdan.bercea98@yahoo.com>
2020-05-05 10:56:26 +0300

    FoodApi added

---
+++ Api's/FoodApi/FoodApi/Controllers/AlimentByNameController.cs
@@ -0,0 +1,34 @@
+﻿using FoodApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApi.Controllers
+{
+    [ApiController]
+    [Route("api/alimentbyname")]
+    public class AlimentByNameController : ControllerBase
+    {
+        private readonly IFoodRepository _foodRepository;
+
+        public AlimentByNameController(IFoodRepository foodRepository)
+        {
+            _foodRepository = foodRepository ??
+                throw new ArgumentNullException(nameof(foodRepository));
+        }
+
+        [HttpGet]
+        [Route("{alimentName}")]
+        public IActionResult GetAlimentByName(string alimentName)
+        {
+            var alimentFromRepo = _foodRepository.GetAlimetByName(alimentName);
+
+            if (alimentFromRepo == null)
+                return NotFound();
+
+            return Ok(alimentFromRepo);
+        }
+    }
+}
